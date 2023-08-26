4d2bd3c59f131e0ec0622d6433633edebaaf8cf8
daniel <danieloliveira.s558@gmail.com>
2023-08-25 22:22:45 -0300

    Quetão 1002 uri erro na saída corrigir

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-//Questão 1000 do uri
+#include<iomanip>
+//Questão 1002 do uri
 using namespace std;
 int main (){
 
-    cout<<"Hellow"<<endl;
+    float n = 3.14159;
+    float raio;
+    float area = 0;
+    cin>>fixed>>setprecision(2);
+    cin>>raio;
+    area = n*raio*raio;
+    cout<<fixed<<setprecision(4)<<endl;
+    cout<<"A="<<area<<endl;
+
     return 0;
 }
